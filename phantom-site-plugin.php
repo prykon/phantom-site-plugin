62d8c13dc5ca229048c979282137154469668eae
Prykon <dariomanoukian@gmail.com>
2021-07-13 16:26:40 -0300

    plugin must not run on a Disciple.Tools instance

--- phantom-site-plugin.php
+++ phantom-site-plugin.php
@@ -22,8 +22,44 @@
   * @return object|bool
   */
  function phantom_site_plugin() {
+ 	 /*
+     * Check if the Disciple.Tools theme is NOT loaded
+     */
+    $wp_theme = wp_get_theme();
+    $is_theme_dt = strpos( $wp_theme->get_template(), "disciple-tools-theme" ) !== false || $wp_theme->name === "Disciple Tools";
+    if ( $is_theme_dt ) {
+        add_action( 'admin_notices', 'phantom_site_plugin_dt_existence_notice' );
+        return false;
+    }
  	return Phantom_Site_Plugin::instance();
  }
+
+if ( ! function_exists( 'phantom_site_plugin_dt_existence_notice' ) ) {
+    function phantom_site_plugin_dt_existence_notice() {
+        $message = 'For security purposes, the Phantom Site Plugin should never coexist in a Disciple.Tools environment. Please install it on a separate, fresh WordPress instance. Thanks!';
+        
+        // Check if it's been dismissed...
+        if ( ! get_option( 'phantom-site-plugin-dt-existence-notice', false ) ) { ?>
+            <div class="notice notice-error phantom-site-plugin-dt-existence-notice" data-notice="phantom-site-plugin">
+                <p><?php echo esc_html( $message );?></p>
+            </div>
+            <script>
+                jQuery(function($) {
+                    $( document ).on( 'click', '.phantom-site-plugin-dt-existence-notice .notice-dismiss', function () {
+                        $.ajax( ajaxurl, {
+                            type: 'POST',
+                            data: {
+                                action: 'dismissed_notice_handler',
+                                type: 'phantom-site-plugin-dt-existence-notice',
+                                security: '<?php echo esc_html( wp_create_nonce( 'wp_rest_dismiss' ) ) ?>'
+                            }
+                        })
+                    });
+                });
+            </script>
+        <?php }
+    }
+}
 
  add_action( 'after_setup_theme', 'phantom_site_plugin' );
 
